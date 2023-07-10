765b42ff9ae11358c242893471f79dfc7e2cf4d1
Laurent0s <dimnik2012kiev@gmail.com>
2023-07-10 19:38:44 +0300

    finalcommit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,7 @@
-import sun from "./../../img/icons/sun.svg";
-import moon from "./../../img/icons/moon.svg";
-
 import { NavLink } from "react-router-dom";
-
+import BtnDarkMode from "../btnDarkMode/btnDarkMode";
 import "./style.css";
+
 
 const Navbar = () => {
 
@@ -19,10 +17,7 @@ const Navbar = () => {
                 </NavLink>
                 
 
-                <button className="dark-mode-btn">
-                    <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-                    <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-                </button>
+                <BtnDarkMode />
 
                 <ul className="nav-list">
                     
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,21 @@
 import img from "./../img/projects/02-big.jpg";
 import BtnGitHub from "../components/btnGitHub/BtnGitHub";
+import {projects} from "./../helpers/projectsList";
+import { useParams } from "react-router-dom";
 
 const Project = () => {
+    const {id} = useParams();
+    const project = projects[id];
+
+    console.log(project);
+
     return ( 
     <main className="section">
         <div className="container">
             <div className="project-details">
 
                 <h1 className="title-1">Video Service</h1>
+                
                 
                 <img src={img} alt="" className="project-details__cover" />
 
